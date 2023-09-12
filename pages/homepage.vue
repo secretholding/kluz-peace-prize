722a775708467e62d6f23b5a6f5d3fecadc8221e
Claudio <claudio@ccmdesign.ca>
2023-09-11 20:03:50 -0700

    adding homepage v0

---
+++ pages/homepage.vue
@@ -0,0 +1,183 @@
+<template>
+  <peacetech-hero />
+  <article>
+    <base-section id="intro">
+      <center-l size="wide">
+        <stack-l class="narrow-content">
+          <h2 class="section__heading">Celebrating Innovation for a <strong>Peaceful World</strong></h2>
+          <p>The Kluz Prize for PeaceTech 2023 celebrates, recognizes, and propels the groundbreaking use of emerging technologies in fostering peace, reducing conflict, and safeguarding human dignity and universal human rights across the globe.</p>
+          <p>The first Kluz Prize for PeaceTech 2022 was awarded to Juan Carlos Lucero, Co-Founder of the Magnolia Foundation, to further his organization’s School of Peace, a multidisciplinary approach to teaching peace, mediation, and rehabilitation.</p>
+        </stack-l>
+      </center-l>    
+    </base-section>
+    <base-section color="primary">
+      <center-l size="wide">
+        <div class="twin-panel">
+          <stack-l>
+            <h2 class="section__heading | color:white">Prize</h2>
+            <p>The award grants selected winner $20,000 to support their achievements and further activities and development of their project in the emerging field of technologies for peace. The Selection Committee will consist of the founding donor, and a group of distinguished international experts and recognized leaders in the fields of peace, technology, investments, and international affairs.</p>
+            <p>This year, the award ceremony will take place on Wednesday, September 20th, on the eve of the <a href="https://internationaldayofpeace.org/" target="_blank">International Day of Peace</a> during the 78th United Nations General Assembly.</p>
+          </stack-l>
+          <div>
+            <img src="assets/images/decor-03.webp" alt="">
+          </div>
+        </div>
+      </center-l>
+    </base-section>
+
+    <base-section textAlign="center">
+      <center-l size="wide">
+        <stack-l>
+          <blockquote class="stack">
+            <p>Technology holds the key to unlocking a more peaceful world, and PeaceTech is the driving force behind this transformative opportunity. Together, by showcasing groundbreaking applications of technology to foster peace, we can accelerate the PeaceTech movement and create tangible, life-changing impact on a global scale.</p>
+            <cite>Artur Kluz, Founder and CEO of Kluz Ventures</cite>
+          </blockquote>
+        </stack-l>
+      </center-l>
+    </base-section>
+
+    <base-section class="bg-decor" >
+      <img src="assets/images/kluz-peacetech-prize-logo-white.svg" alt="" class="center:all">
+    </base-section>
+
+    <base-section>
+      <center-l size="wide">
+        <div class="twin-panel">
+          <stack-l>
+            <h2 class="section__heading">About</h2>
+            <p>The Kluz Prize for PeaceTech is an initiative to celebrate, distinguish, and promote the use of new technologies in fostering peace, reducing conflict, and protecting human dignity and universal human rights across the globe. The Prize is dedicated to individuals, organizations, or initiatives for their distinguished achievements in peacebuilding, peacemaking, peacekeeping, or peace enforcement efforts.  The goal of the Prize is to engage individuals, societies, and states in building up a local, national, and global PeaceTech movement.</p>
+          </stack-l>
+          <div>
+            <img src="assets//images/decor-02.webp" alt="">
+          </div>
+        </div>
+      </center-l>
+    </base-section>
+
+    <base-section textAlign="center">
+      <center-l size="wide">
+        <stack-l>
+          <h2 class="section__heading">In particular, the Prize seeks to:</h2>
+          <cluster-l>
+            <stack-l class="prize-theme"><img src="assets/images/theme-icon-heart.svg" alt=""><p>Recognize and reward groundbreaking PeaceTech achievements</p></stack-l>
+            <stack-l class="prize-theme"><img src="assets/images/theme-icon-smile.svg" alt=""><p>Encourage collaboration among global leaders, peacebuilders, technologists, entrepreneurs, investors, and policymaker</p></stack-l>
+            <stack-l class="prize-theme"><img src="assets/images/theme-icon-star.svg" alt=""><p>Raise awareness about the potential of technology in fostering peace</p></stack-l>
+            <stack-l class="prize-theme"><img src="assets/images/theme-icon-check.svg" alt=""><p>Engage individuals, societies, states, international organizations, and by and large new generations of young people in building up a local, national, and global PeaceTech movement</p></stack-l>
+          </cluster-l>
+        </stack-l>
+      </center-l>    
+    </base-section>
+
+    <base-section>
+      <center-l size="wide">
+        <stack-l space="var(--s3)">
+          <h3>"PeaceTech" encapsulates the strategic and responsible deployment of innovative technologies to reinforce peace, prevent conflicts, and steer peacebuilding initiatives. It harnesses, for instance:</h3>
+          <ul class="columns">
+            <li class="columns-item">Artificial Intelligence (AI) and Machine Learning (ML), predicting conflicts, enhancing decision-making, facilitating mediation, and assessing responses to societal issues</li>
+            <li class="columns-item">Predictive modeling, spotting early conflict indicators for timely interventions</li>
+            <li class="columns-item">Satellite and drone imaging, serving multifaceted roles from conflict prevention to post-war reconstruction</li>
+            <li class="columns-item">Social media, monitoring for potential social unrest, and providing vital communication in conflict zones</li>
+            <li class="columns-item">Digital communication platforms, fostering dialogue, promoting understanding, and mediating conflicts</li>
+            <li class="columns-item">Crowdsourcing and citizen reporting, tapping into collective intelligence for conflict mapping, violence reporting, and peace initiatives</li>
+            <li class="columns-item">Digital educational tools, spreading knowledge on peacebuilding and conflict resolution</li>
+            <li class="columns-item">3D printing, meeting essential supply demands swiftly in conflict-stricken regions</li>
+            <li class="columns-item">Biometrics, enabling fair democratic processes like elections and referenda, and facilitating targeted aid distribution</li>
+            <li class="columns-item">Robotics, offering surveillance capabilities, and aid delivery to displaced populations, minimizing wars and fostering peace</li>
+          </ul>
+          <p visual="h3"> 
+            These are just a few examples of PeaceTech. The field is continually evolving as new technologies and methodologies are developed. Learn more about the key themes and challenges of technologies used by and created for peace efforts in 
+            <a href="">“PeaceTech Topic Map: A Research Base for an Emerging Field”</a>.
+          </p>
+        </stack-l>
+      </center-l>
+    </base-section>
+
+    <base-section color="primary">
+      <center-l size="wide">
+        <div class="twin-panel">
+          <stack-l>
+            <h2 class="section__heading">Kluz Ventures</h2>
+            <p>Kluz Ventures is a values-driven investment firm with a long-term vision of empowering human flourishing and technological development by investing in the exceptional entrepreneurs and technologies of tomorrow. Investments in these technologies can promote a global culture of genuine human growth that meets the current and future needs of all people, serving as an inspiration for future generations. Founded by Artur Kluz.</p>
+            <p><base-button el="a" color="accent" visual="primary" href="https://www.kluzventures.com/" target="_blank">Learn More</base-button></p>
+          </stack-l>
+          <div><img src="assets/images/kluz-ventures.webp" alt=""></div>
+        </div>
+      </center-l>    
+    </base-section>
+    
+    <base-section>
+      <center-l size="wide">
+        <div class="twin-panel">
+          <stack-l>
+            <p>The GovLab is the main partner supporting the Kluz Prize for PeaceTech, and is an action research center based at New York University’s that aims to improve people's lives by changing the way we govern. The GovLab seeks to strengthen the ability of institutions — including but not limited to governments — and people to work more openly, collaboratively, effectively, and legitimately to make better decisions and solve public problems.</p>
+            <p><base-button el="a" color="accent" visual="primary" href="https://thegovlab.org/" target="_blank">More Information</base-button></p>
+          </stack-l>
+          <img src="assets/images/govlab-logo.svg" alt="">
+        </div>
+      </center-l>    
+    </base-section>
+    
+    <base-section color="primary">
+      <center-l size="wide">
+        <blockquote>
+          <p>Apply now to be recognized for your achievements and contributions to the evolving field of technologies for peace. In particular, we are seeking to learn about impactful and innovative initiatives that leverage technology to, for instance</p>
+        </blockquote>
+      </center-l>    
+    </base-section>
+  </article>
+  
+  <base-section>
+      <center-l size="wide">
+        <div class="twin-panel">
+          <stack-l>
+            <img src="assets/images/kluz-peace-prize-logo.svg" alt="">
+            <ul>
+              <li><NuxtLink to="/">Home</NuxtLink></li>
+              <li><NuxtLink to="/agenda">Agenda</NuxtLink></li>
+              <li><NuxtLink to="/people">Jury and Panelists</NuxtLink></li>
+            </ul>
+          </stack-l>
+          <stack-l>
+            <p><strong>Applications are closed</strong></p>
+            <p>For more information, please get in touch with <a href="mailto:info@kluzprize.org">info@kluzprize.org</a></p>
+            <p>The GovLab and its partners are committed to respecting your privacy. We abide by all the practices and principles outlined by New York University in its Digital Privacy Statement. For more information, please <a href="https://www.nyu.edu/footer/copyright-and-fair-use/digital-privacy-statement.html" target="_blank">review the full statement here</a>.</p>
+          </stack-l>
+        </div>
+      </center-l>    
+  </base-section>
+
+  <footer class="background-color:primary padding:s0">
+    <p class="text-align:center color:white"><strong>© 2023 by Kluz Ventures</strong></p>
+  </footer>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+.prize-theme {
+  box-sizing: border-box;
+  max-width: calc(25% - var(--s0));
+  padding: var(--s0);
+  align-self: flex-start;
+
+  img {
+    max-width: 120px;
+    margin-inline: auto;
+    height: 100px;
+  }
+}
+
+.bg-decor {
+  background-image: url(assets/images/decor-01.webp);
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+
+  img {
+    max-width: 600px;
+    width: 90%;
+  }
+}
+</style>
