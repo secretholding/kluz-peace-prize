245ef4fd562d240cca608a8b1675f43d8bd60d26
Claudio <claudio@ccmdesign.ca>
2024-05-21 13:06:30 -0700

    homepage sections

--- components/sections/kppHighlights.vue
+++ components/sections/kppHighlights.vue
@@ -2,13 +2,14 @@
   <kpp-base-section :content="content">
     <center-l size="wide">
       <h2>{{ content.title }}</h2>
+      <div class="updates-highlights | margin-top:s2">
+        <kpp-grid-item v-for="i in content.posts" class="updates-highlights__item" :content="i" />
+      </div>
     </center-l>
-    <div class="updates-highlights | margin-top:s2">
-      <kpp-grid-item v-for="i in content.posts" class="updates-highlights__item" :content="i" />
-    </div>
+    
     <center-l class="margin-top:s2 text-align:center">
-      <nuxt-link :to="content.action.url" class="nocolor">
-          <base-button el="NuxtLink" size="l">{{ content.action.label }}</base-button>
+      <nuxt-link :to="content.action.url" unstyled="color">
+        <base-button el="NuxtLink" size="l">{{ content.action.label }}</base-button>
       </nuxt-link>
     </center-l>
   </kpp-base-section>
@@ -28,21 +29,13 @@ const props = defineProps({
 <style lang="scss" scoped>
 .updates-highlights {
   display: grid;
-  grid-template-columns: repeat(3, minmax(300px, 1fr));
-
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(1, 1fr);
-  }
+  gap: var(--s1);
+  grid-template-columns: repeat(2, minmax(300px, 1fr));
+  @media (max-width: 768px) { grid-template-columns: repeat(1, 1fr); }
 }
 
 .highlights-reel__item {
   --n: 1;
   --d: 1;
 }
-
-.nocolor {
-  color: unset;
-}
-
-
 </style>
