6127c1d2eca641ddd0131ff4819a1952a0c8b909
Douglas Cantão <dcp.pereira@gmail.com>
2024-05-21 17:18:55 -0300

    Small fix on prose prop name

--- components/modules/kppProse.vue
+++ components/modules/kppProse.vue
@@ -1,14 +1,14 @@
 <template>
   <kluz-prose>
     <slot >
-      <div v-html="HtmlContent"></div>
+      <div v-html="htmlContent"></div>
     </slot>
   </kluz-prose>
 </template>
 
 <script setup>
   defineProps({
-    HtmlContent: {
+    htmlContent: {
       type: String,
       default: ''
     }
