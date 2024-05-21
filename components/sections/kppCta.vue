5afd7728a3d56f7a98cde16c68ad9630ed0dc2a6
Claudio <claudio@ccmdesign.ca>
2024-05-21 15:32:03 -0700

    kpp-cta revamp

--- components/sections/kppCta.vue
+++ components/sections/kppCta.vue
@@ -1,5 +1,5 @@
 <template>
-  <kpp-base-section class="event-cta" height="auto" color="primary">
+  <kpp-base-section class="kpp-cta" height="auto" :color="color">
     <center-l size="wide">
       <div class="wrapper">
         <stack-l class="panel panel--header">
@@ -18,11 +18,17 @@
 </template>
 
 <script setup>
+const props = defineProps({
+  color: {
+    type: String,
+    default: 'base'
+  }
+});
 
 </script>
 
 <style lang="scss" scoped>
-.event-cta {
+.kpp-cta {
   padding-block: var(--s3);
   @media (min-width: 768px) { padding-block: var(--s3); }
 }
@@ -31,11 +37,8 @@
   display: flex;
   align-items: center;
   flex-direction: column;
+  gap: var(--s2);
   @media (min-width: 768px) { flex-direction: row; }
-}
-
-.panel {
-//  flex: 1; // max-width: 50%;
 }
 
 .panel--header {
@@ -43,8 +46,55 @@
   flex: 5;
 }
 
+.kpp-cta {
+  .brow {
+    font-size: 150%;
+    @media screen and (max-width: 768px) { font-size: 100%; }
+    line-height: 1;
+    font-weight: 800;
+    text-transform: uppercase;
+    color: var(--primary-color);
+  }
+  .title {
+    font-size: 400%;
+    @media screen and (max-width: 768px) { font-size: 200%; }
+    line-height: 1;
+    --space: 0;
+  }
+
+  p { --space: var(--s1); }
+}
+
+
+
+
+
 .panel--action {
   flex: 2;
   > * { margin-left: auto; }
+}
+
+.kpp-cta[color="base"] {
+  background-color: var(--base-color);
+  color: var(--white-color);
+}
+
+.kpp-cta[color="primary"] {
+  background-color: var(--primary-color);
+  color: var(--white-color);
+}
+.kpp-cta[color="secondary"] {
+  background-color: var(--secondary-color);
+  color: var(--base-color);
+  
+}
+.kpp-cta[color="tertiary"] {
+  background-color: var(--tertiary-color);
+  color: var(--primary-color);
+  
+}
+.kpp-cta[color="accent"] {
+  background-color: var(--accent-color);
+  color: var(--base-color);
 }
 </style>
