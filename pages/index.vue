c6254addc0a1e6c56b65b3e394a465dc9a55c0d4
Douglas Cantão <dcp.pereira@gmail.com>
2024-05-22 18:28:28 -0300

    Sorting home events

--- pages/index.vue
+++ pages/index.vue
@@ -28,9 +28,9 @@ const updatesHighlights = {
     url: '/updates'
   }
 }
-
+let posts = {}
 const events = await queryContent('events').find();
-const groupedByYear = (events.reduce((grouped, event) => {
+let groupedByYear = (events.reduce((grouped, event) => {
   const year = event.year; // replace 'year' with the actual attribute name in your event object
   if (!grouped[year]) {
     grouped[year] = {};
@@ -39,11 +39,15 @@ const groupedByYear = (events.reduce((grouped, event) => {
   return grouped;
 }, {}));
 
+let order = Object.keys(groupedByYear).sort((a, b) => b - a)
+order.forEach((year) => {
+  posts[`'${year}'`] = groupedByYear[year]
+})
 
 const eventsHighlights = {
   title: 'Events Highlights',
   // ToDo: Inverter a ordem dos eventos. 2023 primeiro, 2022 depois.
-  posts: groupedByYear,
+  posts: posts,
   action: {
     label: 'View All Events',
     url: '/events'
