a6f7164c1c8d808ad83a2720cc777e6b205effe4
Seidgy Amano <seidgy@gmail.com>
2024-05-24 17:30:59 -0300

    Slider fixes

--- components/sections/kppSlider.vue
+++ components/sections/kppSlider.vue
@@ -134,7 +134,7 @@ onUnmounted(() => {
     top: 0;
     bottom: 0;
     right: 0;
-    background-image: linear-gradient(0deg, transparent 0%, var(--base-color-70) 70%, var(--base-color-70) 100%);
+    background-image: linear-gradient(180deg, transparent 0%, var(--base-color-70) 70%, var(--base-color-70) 100%);
   }
 }
 
@@ -171,7 +171,7 @@ onUnmounted(() => {
     border: none;
     border-radius: 50%;
     background-color: var(--white-color-70);
-    transition: all 0.2s ease-in-out;
+    transition: all 0.4s ease-in-out;
   }
 
     .slider-nav__trigger--active {
