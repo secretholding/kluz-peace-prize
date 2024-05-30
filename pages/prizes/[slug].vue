08c46ea618460c2e0b7d78ae994b4c8411ff0e8c
Claudio <claudio@ccmdesign.ca>
2024-05-30 13:28:17 -0700

    metrics source fallback

--- pages/prizes/[slug].vue
+++ pages/prizes/[slug].vue
@@ -63,7 +63,8 @@
               <span class="metrics__label">{{ i.label }}</span>
             </li>
           </ul>
-          <p v-if="winner.impact.metrics_source" class="metrics_source | text-align:center">Source: <a :href="winner.impact.metrics_source.url" target="_blank">{{ winner.impact.metrics_source.label }}</a></p>
+          <p v-if="winner.impact.metrics_source" class="metrics_source">Source: <a :href="winner.impact.metrics_source.url" target="_blank">{{ winner.impact.metrics_source.label }}</a></p>
+          <p v-else class="metrics_source">Source: Provided in the interview above</p>
         </div>
       </center-l>
     </kpp-base-section>
