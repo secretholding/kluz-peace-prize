79dea3a36323c437d1af850f0a6e4d31bb9baa8f
Claudio <claudio@ccmdesign.ca>
2024-07-11 14:58:39 -0700

    kpp-slide file

---
+++ components/kpp-slide.vue
@@ -0,0 +1,38 @@
+<template>
+  <component 
+    :content="content" 
+    :is="isVideo" 
+    class="winner-image" 
+    :src="`/assets/images/winners/${slug}/${content}`" 
+    alt=""
+    :controls="isVideo === 'video'"
+    :muted="isVideo === 'video'"
+  >
+  </component>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue';
+
+const props = defineProps({
+  content: {
+    type: String,
+    default: ""
+  },
+  slug: {
+    type: String,
+    default: ""
+  }
+});
+
+const isVideo = computed(() => {
+  return props.content.endsWith('.mp4') ? 'video' : 'img';
+});
+
+</script>
+
+<style lang="scss" scoped>
+.winner-image {
+  max-height: 400px;
+}
+</style>
