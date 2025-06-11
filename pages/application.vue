6fb5b8d4f146aa892b396fec7900f05d0c63e2cd
Secret Holding <secretholding@users.noreply.github.com>
2025-06-11 14:42:46 -0700

    Update application.vue

--- pages/application.vue
+++ pages/application.vue
@@ -82,11 +82,11 @@
             <legend>If you are awarded the KluzPrize, would you be available to attend an awards ceremony in New York City on Friday, September 19th?</legend>
             <div>
               <input type="radio" id="attendance" value="yes" checked />
-              <label for "yes">Yes</label>
+              <label for "yes" />Yes</label>
             </div>
             <div>
               <input type="radio" id="attendance value="no" />
-              <label for "no">No</label>
+              <label for "no" />No</label>
             </div>
 
             <p class="margin-bottom:s2"><strong class="color:primary">Do you have any pictures or videos that support making your technology come to life? Please email them to us at <a href="mailto:info@kluzprize.org" target="_blank" class="color:primary">info@kluzprize.org</a> with your name as the subject line.</strong></p>
